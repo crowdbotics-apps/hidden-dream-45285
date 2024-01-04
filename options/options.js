c98fbb06ea085cc24664e18120a2056681508956
Crowdbotics <team@crowdbotics.com>
2024-01-04 23:04:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-dream-45285",
+  url: "https://hidden-dream-45285.botics.co",
+  api: "https://hidden-dream-45285.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
